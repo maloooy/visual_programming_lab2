ab98ae2730965ce455b265f7c8c6f4866047c399
Mikhail <basargin123@gmail.com>
2022-02-24 20:21:30 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // RomanNumber nr1 = new RomanNumber(0); 
+                RomanNumber nr1 = new RomanNumber(82);
+                RomanNumber nr2 = new RomanNumber(18);
+                RomanNumber nr3 = new RomanNumber(32);
+                // RomanNumber nr3 = new RomanNumber(92);
+                RomanNumber nr4 = new RomanNumber(2);
+                RomanNumber nr5 = new RomanNumber(41);
+                Console.WriteLine($"addition: {nr1.ToString()} + {nr2.ToString()} = {RomanNumber.Add(nr1, nr2).ToString()}");
+                Console.WriteLine($"subtraction: {nr1.ToString()} - {nr3.ToString()} = {RomanNumber.Sub(nr1, nr3).ToString()}");
+                Console.WriteLine($"multiplication: {nr1.ToString()} * {nr4.ToString()} = {RomanNumber.Mul(nr1, nr4).ToString()}");
+                Console.WriteLine($"division: {nr1.ToString()} / {nr5.ToString()} = {RomanNumber.Div(nr1, nr5).ToString()}");
+                RomanNumber[] arr = { nr1, nr2, nr3, nr4, nr5 };
+                Array.Sort(arr);
+                Console.WriteLine("sort result");
+                foreach (var i in arr)
+                {
+                    Console.WriteLine(i.ToString());
+                }
+
+            }
+            catch (RomanNumberException exc)
+            {
+                Console.WriteLine($"error: {exc.Message}");
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp3/RomanNumber.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class RomanNumber : ICloneable, IComparable
+    {
+        private ushort number;
+        private static int[] val = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        private static string[] romanian = new string[]
+            { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        public RomanNumber(ushort n)
+        {
+            if (n <= 0) throw new RomanNumberException($"number {n} is less or equal to 0");
+            else this.number = n;
+        }
+        public static RomanNumber Add(RomanNumber? n1, RomanNumber? n2)
+        {
+            int num = n1.number + n2.number;
+            if (num <= 0) throw new RomanNumberException("failed to add numbers");
+            else
+            {
+                RomanNumber result = new((ushort)num);
+                return result;
+            }
+        }
+        public static RomanNumber Sub(RomanNumber? n1, RomanNumber? n2)
+        {
+            int num = n1.number - n2.number;
+            if (num <= 0) throw new RomanNumberException("subtraction result is less or equal to 0");
+            else
+            {
+                RomanNumber result = new((ushort)num);
+                return result;
+            }
+        }
+        public static RomanNumber Mul(RomanNumber? n1, RomanNumber? n2)
+        {
+            int num = n1.number * n2.number;
+            if (num <= 0) throw new RomanNumberException("failed to multiply numbers");
+            else
+            {
+                RomanNumber result = new((ushort)num);
+                return result;
+            }
+        }
+        public static RomanNumber Div(RomanNumber? n1, RomanNumber? n2)
+        {
+
+            if (n2.number == 0) throw new RomanNumberException("division error");
+            else
+            {
+                int num = n1.number / n2.number;
+                if (num == 0) throw new RomanNumberException("division error");
+                else
+                {
+                    RomanNumber result = new((ushort)num);
+                    return result;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            int temp = number;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < 13; i++)
+            {
+                while (temp >= val[i])
+                {
+                    temp -= (ushort)val[i];
+                    result.Append(romanian[i]);
+                }
+            }
+            if (result.ToString() == "")
+                throw new RomanNumberException("conversion to roman numerals is not possible");
+            else
+                return result.ToString();
+
+        }
+
+        public object Clone()
+        {
+            return new RomanNumber(number);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is RomanNumber roman)
+                return number.CompareTo(roman.number);
+            else
+                throw new RomanNumberException("object is not a RomanNumber");
+        }
+
+    }
+
+}
